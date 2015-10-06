ef64104d92037241a6bad32c9c5848bed6d476a5
nikoles99 <nikido@tut.by>
2015-10-06 19:29:46 +0300

    compare files

--- 1_sample.asm
+++ 1_sample.asm
@@ -2,54 +2,147 @@
 .stack 
 .data                  
 
-file_name db 'file.txt',0    
-p_fname   dd file_name   
+file_1     db 'file1.txt',0    
+p_file_1   dd file_1  
 
-s_error   db 'Error!',13,10,'$' 
-p_s_error   dd s_error       
+file_2     db 'file2.txt',0    
+p_file_2   dd file_2  
 
-buffer    db 2048 dup(0)   
-p_buff    dd buffer 
+file_3     db 'file3.txt',0    
+p_file_3   dd file_3       
+
+error      db 'Error!',13,10,'$' 
+p_error    dd error       
+
+buffer_1     db 4096 dup(0)   
+p_buffer_1   dd buffer_1   
+
+buffer_2     db 4096 dup(0)   
+p_buffer_2   dd buffer_2 
+
+buffer_3     db 4096 dup(0)   
+p_buffer_3   dd buffer_3 
         
-handle    dw 0 
-.code          
-     jmp start 
+handle     dw 0   
 
-start:
+str1        db 'qwsdsderty'
+str2        db 'qwerty'
+len         = $ - str2
+mess        db 'yes$'    
+mess2        db 'no$'   
+
+.code          
+     jmp begin 
+
+begin:
     mov ax, @data
     mov ds, ax
+    call openFile1
+    call openFile2
+    call openFile3            
+    jmp exit
+    
+openFile1:   
     mov ah,3Dh              
     xor al,al               
-    mov dx,p_fname  
-    int 21h                 
-    jnc openFile                  
+    lea dx, file_1
+    int 21h
+    jnc readFile1                  
     call error_msg          
-    jmp exit    
+    jmp exit  
+
+openFile2:   
+    mov ah,3Dh              
+    xor al,al               
+    lea dx, file_2
+    int 21h
+    jnc readFile2                  
+    call error_msg          
+    jmp exit 
+    
+openFile3:   
+    mov ah,3Dh              
+    xor al,al               
+    lea dx, file_3
+    int 21h 
+    jnc readFile3                  
+    call error_msg          
+    jmp exit      
                 
-openFile: mov [handle],ax         
+readFile1:
+    mov [handle],ax         
     mov bx,ax               
     mov ah,3Fh              
-    mov dx,p_buff           
+    mov dx,p_buffer_1           
     mov cx,80               
     int 21h                 
-    jnc printPile                
+    jnc printFile                
     call error_msg          
-    jmp close_file          
+    jmp close_file  
+    
+readFile2:
+    mov [handle],ax         
+    mov bx,ax               
+    mov ah,3Fh              
+    mov dx,p_buffer_2           
+    mov cx,80               
+    int 21h                 
+    jnc printFile                
+    call error_msg          
+    jmp close_file
+    
+readFile3:
+    mov [handle],ax         
+    mov bx,ax               
+    mov ah,3Fh              
+    mov dx,p_buffer_3           
+    mov cx,80               
+    int 21h                 
+    jnc compareFile               
+    call error_msg          
+    jmp close_file        
  
-printPile: mov bx,p_buff
+printFile:
+    mov bx,p_buffer_1
     add bx,ax               
     mov byte[bx],'$'         
     mov ah,9
-    mov dx,p_buff
+    mov dx,p_buffer_1
     int 21h 
 
 close_file:
     mov ah,3Eh              
     mov bx,[handle]         
-    int 21h                 
-    jnc exit                
-    call error_msg          
- 
+    int 21h
+    xor dx,dx
+    ret                           
+
+
+compareFile:
+    mov     ax,@data
+    mov     ds,ax
+    mov     es,ax
+    
+    mov     cx,len
+    mov     si,offset p_buffer_1
+    mov     di, offset p_buffer_2
+    repe cmpsb
+    jne     NO
+    
+Yes:    
+    mov     ah,9
+    mov     dx,offset mess
+    int     21h   
+    call    exit  
+    
+NO:   
+    mov     ah,9
+    mov     dx,offset mess2
+    int     21h  
+    mov     ax,4c00h
+    int     21h      
+    
+    
 exit:      
     mov ah,8                
     int 21h                 
@@ -58,6 +151,7 @@ exit:
   
 error_msg:
     mov ah,9
-    mov dx,p_s_error
+    mov dx,p_error
     int 21h                 
     ret
+
